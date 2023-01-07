706a69ab9e7e7c6198ca403dd0fbfe8b5b4e1808
Igor Novikov <igor.novikov@umbrellait.com>
2023-01-07 09:25:20 +0300

    diffutil

--- rw2/src/main/java/com/app/rw2/adapter/UserDiffCallBack.kt
+++ rw2/src/main/java/com/app/rw2/adapter/UserDiffCallBack.kt
@@ -9,6 +9,14 @@ class UserDiffCallBack(private val oldList: List<User>, private val newList: Lis
 
     override fun getNewListSize(): Int = newList.size
 
+    /** Тут используется аналогия с equals и hashcode. Если hashcode одинаковый то не факт что это два одинаковых объекта и мы дополнительно
+     * проверяем поля с помощью equals. Тут тоже самое если areItemsTheSame вернул true - дополнительно проверяем не изменились ли
+     * некоторые данные viewholder с помощью areContentsTheSame. Если один из методов вернет false - значет будет вызван
+     * onBindViewholder() и данный элемент перерисуется.
+     *
+     * Подробно об этом можно почитать здесь - https://startandroid.ru/ru/blog/504-primer-ispolzovanija-android-diffutil.html
+     * */
+
     // Проверяем элементы старого и нового списка на предмет того это одни и теже элементы или нет
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         val oldUser = oldList[oldItemPosition]
@@ -16,7 +24,7 @@ class UserDiffCallBack(private val oldList: List<User>, private val newList: Lis
         return oldUser.id == newUser.id // В большенстве случаев проверка происходит по id (или НАВЕРНОЕ по другим данных если id недостаточно)
     }
 
-    // Проверяем содержимое элементов
+    // Проверяем содержимое элементов если areItemsTheSame вернул true. Проверяем поменялось ли содержимое
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         val oldUser = oldList[oldItemPosition]
         val newUser = newList[newItemPosition]
@@ -25,5 +33,3 @@ class UserDiffCallBack(private val oldList: List<User>, private val newList: Lis
     /** Важный ньюанс. == отработает только для data классов т.к там переопределен метод equals.
      * Для обычных классов мы сами должны переопределить equals*/
 }
-
-// TODO: Тут работает аналогия с equals и hashCode. Hashcode используется для оптимизации чтобы постоянно не вызывать equals. Нужно разобраться есть ли тут эта идея и когда вызовется 2 метод.
